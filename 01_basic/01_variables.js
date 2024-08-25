06c9177186d5f05d1e1311a3a38018d250150446
Mehul Singh <thakurmehul492@gmail.com>
2024-08-25 04:13:43 +0000

    variables declares

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14455345
+let accountEmail = "mehulsingh@gmail.com"
+var accountPassword = "xxx24324"
+accountCity = "Agra"
+let accountState;
+
+//accountId = 2 // not allowed
+accountEmail ="mc@gmail.com"
+accountPassword = "2345"
+accountCity = "Banglore"
+
+//console.log(accountId);//
+/*
+perfer not to use var
+because of issue in boock scope and functional scope
+*/
+
+
+console.log([accountEmail, accountId, accountCity,accountPassword,accountState]);
