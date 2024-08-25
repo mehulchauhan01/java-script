627f638824fef531edf149e16c880f050e69295e
Mehul Singh <thakurmehul492@gmail.com>
2024-08-25 04:16:58 +0000

    conversations

---
+++ 01_basic/03_conversertion.js
@@ -0,0 +1,33 @@
+let score = "true"
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" =>Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggendIn = Bololean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+//"mehul" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringnumber);
+//console.log(typeof stringNumber);
+
+
+//***************************     Operations     *******************************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
