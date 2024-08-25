1cb8b16470605d4ce486d9e199b0582dfe42c158
Mehul Singh <thakurmehul492@gmail.com>
2024-08-25 04:15:27 +0000

    Data-types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+const accountId = 14455345
+let accountEmail = "mehulsingh@gmail.com"
+var accountPassword =  "xx12213"
+ accountCity = "Agra"
+ let accountState; 
+
+//accountId = 2 // not allowed
+accountEmail = "mc@.com"
+accountPassword = "23233"
+accountCity = "Banglore"
+//console.log(accountId);//
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+
+console.log([accountEmail, accountId, accountCity, accountState]);
