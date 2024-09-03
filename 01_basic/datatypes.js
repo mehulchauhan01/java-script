370afeedbce0f41b8ceec91f23907b5813647d9b
Mehul Singh <thakurmehul492@gmail.com>
2024-09-03 05:19:45 +0000

    Datatype summary

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,37 @@
+// primitive datatypes
+
+// 7 types : String , Number , Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 120
+
+const isLoggedIn = false
+const outsideTemp = null ;
+let userEmail;
+
+
+const id = symbol('1234')
+const anotherId = symbol('1234')
+console.log(id === anotherId);
+
+const bigNumber = 58746873684658n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions,
+
+
+const heros = ["saktiman", "naagraj","doga"]
+let myObj ={
+    name: "Mehulsingh",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+} 
+
+console.log(typeof myFunction);
+
+
+//https://ecma-international.org/
